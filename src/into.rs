e8a38f5a51571a2d29a9c573cf1e0705f7926eda
Hsingai Altaica <draltaica@yahoo.com>
2024-03-02 05:50:31 -0800

    added Into<f32> and Into<f64> for &Fraction

--- src/into.rs
+++ src/into.rs
@@ -1,6 +1,6 @@
 use crate::Fraction;
 
-impl Into<f32> for Fraction {
+impl Into<f32> for &Fraction {
     fn into(self) -> f32 {
         let x = self.numer as f32 / self.denom.get() as f32;
         match self.sign {
@@ -9,14 +9,24 @@ impl Into<f32> for Fraction {
         }
     }
 }
+impl Into<f32> for Fraction {
+    fn into(self) -> f32 {
+        (&self).into()
+    }
+}
 
 
-impl Into<f64> for Fraction {
+impl Into<f64> for &Fraction {
     fn into(self) -> f64 {
         let x = self.numer as f64 / self.denom.get() as f64;
         match self.sign {
             crate::Sign::Positive => x,
             crate::Sign::Negative => -x,
         }
+    }
+}
+impl Into<f64> for Fraction {
+    fn into(self) -> f64 {
+    (&self).into()
     }
 }
