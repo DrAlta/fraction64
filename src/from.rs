6dcefd63d0db8f780fb7885244f758acffe29ff4
Hsingai Altaica <draltaica@yahoo.com>
2024-03-02 04:16:46 -0800

    Added From<(i64, u64)> also discoved .unsigned_abs() so changed to use that

--- src/from.rs
+++ src/from.rs
@@ -23,6 +23,22 @@ impl From<u64> for Fraction {
         Self {sign: Sign::Positive, numer: item.into(), denom: NonZeroU64::new(1_u64).unwrap()}
     }
 }
+impl From<(i64, u64)> for Fraction {
+    fn from(item: (i64, u64)) -> Self {
+        let Some(denom) = NonZeroU64::new(item.1) else {
+            // any amoutntht of zeroth in zero;
+            return Self::ZERO
+        };
+        let sign = if item.0.is_positive(){
+            Sign::Positive
+        } else {
+            Sign::Negative
+        };
+
+        let numer = item.0.unsigned_abs();
+        Self {sign, numer, denom}
+    }
+}
 
 
 ////////
@@ -62,13 +78,13 @@ impl From<i32> for Fraction {
 }
 impl From<i64> for Fraction {
     fn from(item: i64) -> Self {
-        let sign = if item < 0 {
-            Sign::Negative
-        } else {
+        let sign = if item.is_positive(){
             Sign::Positive
+        } else {
+            Sign::Negative
         };
 
-        Self {sign, numer: item.abs() as u64, denom: NonZeroU64::new(1_u64).unwrap()}
+        Self {sign, numer: item.unsigned_abs(), denom: NonZeroU64::new(1_u64).unwrap()}
     }
 }
 
