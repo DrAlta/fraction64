62b804eebf37ce5b18cb859f59d57d92b881e058
Hsingai Altaica <draltaica@yahoo.com>
2024-12-17 13:16:35 -0800

    added more consts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+2024-12-17
+
+added Fraction::FIVE, Fraction::NEG_THREE, Fraction::NEG_FOUR, Fraction::NEG_FIVE, Fraction::NEG_SIX, 
+
 2024-09-20
 
 added implemented Into<f32> and Into<f64> for Fraction
--- src/consts.rs
+++ src/consts.rs
@@ -2,6 +2,10 @@ use std::num::NonZeroU64;
 
 use super::{Fraction, Sign};
 impl Fraction {// 18_446_744_073_709_551_615
+    pub const NEG_SIX: Self = unsafe {Fraction{sign: Sign::Negative, numer: 6_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
+    pub const NEG_FIVE: Self = unsafe {Fraction{sign: Sign::Negative, numer: 5_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
+    pub const NEG_FOUR: Self = unsafe {Fraction{sign: Sign::Negative, numer: 4_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
+    pub const NEG_THREE: Self = unsafe {Fraction{sign: Sign::Negative, numer: 3_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
     pub const NEG_TWO: Self = unsafe {Fraction{sign: Sign::Negative, numer: 2_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
     pub const NEG_FOUR_THIRDS: Fraction = unsafe {Fraction::new_neg(4, std::num::NonZeroU64::new_unchecked( 3))};
     pub const NEG_ONE: Self = unsafe {Fraction{sign: Sign::Negative, numer: 1_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
@@ -17,6 +21,7 @@ impl Fraction {// 18_446_744_073_709_551_615
     pub const FOUR_THIRDS: Fraction = unsafe {Fraction::new(4, std::num::NonZeroU64::new_unchecked( 3))};
     pub const THREE: Self = unsafe {Fraction{sign: Sign::Positive, numer: 3_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
     pub const FOUR: Self = unsafe {Fraction{sign: Sign::Positive, numer: 4_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
+    pub const FIVE: Self = unsafe {Fraction{sign: Sign::Positive, numer: 5_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
     pub const SIX: Self = unsafe {Fraction{sign: Sign::Positive, numer: 6_u64, denom: NonZeroU64::new_unchecked(1_u64)}};
     
     pub const E: Self = unsafe {Fraction{sign: Sign::Positive, numer: 5_739_439_214_861_417_731_u64, denom: NonZeroU64::new_unchecked(2_111_421_691_000_680_031_u64)}};
